d1e01f872b83451bcd334f58e815bfc46f2477f3
Ryan <guillemertinjus@gmail.com>
2025-04-14 21:17:28 +0800

    Final commit

--- vehicle.java
+++ vehicle.java
@@ -1,4 +1,15 @@
 public class vehicle {
+private String brand;
+private int speed;
+private String FuelType;
+
+public Vehicle(String brand, int speed, String FuelType) {
+    this.brand = brand;
+    this.speed = speed;
+    this.FuelType = FuelType;
+
+    
+}
 
     
 }
